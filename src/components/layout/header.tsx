a2e1336a7323978438102fffc61dbe3fc1c36f32
Varshath Gupta S <varshathgupta@gmail.com>
2025-08-16 18:45:38 +0000

    Add darker color for header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@ import { UserNav } from './user-nav';
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
+    <header className="sticky top-0 z-40 w-full border-b bg-card">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <Building className="size-8 text-primary" />
